bf0e786d62fbff8f162582ec6f047834dd826785
AdamAvnet <aavnet20@stuy.edu>
2021-01-17 16:32:25 -0500

    Scans the list of cities and adds them to Distance objects which are then added to an arraylist.

---
+++ TravelingSalesman.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class TravelingSalesman{
+
+	public static void PossibleRoutes(ArrayList<Distance> cities){
+		
+	}
+
+	public static void main(String[] args){
+		ArrayList<Distance> cities = new ArrayList<Distance>();
+		Scanner Sca = new Scanner(System.in);
+		String origin = "";
+		String destination = "";
+		int distance = 0;
+		while(Sca.hasNextLine()){
+			String nline = Sca.nextLine();
+			Scanner Scar = new Scanner(nline);
+			int wordcount = 0;
+			while(Scar.hasNext()){
+				String word = Scar.next();
+				if(wordcount == 0)
+					origin = word;
+				if(wordcount == 2)
+					destination = word;
+				if(wordcount == 4)
+					distance = Integer.parseInt(word);
+				wordcount += 1;
+			}
+			Distance city = new Distance(origin, destination, distance);
+			cities.add(city);
+		}
+		for(int i = 0; i < cities.size(); i++){
+			System.out.println(cities.get(i));
+		}
+	}
+}
